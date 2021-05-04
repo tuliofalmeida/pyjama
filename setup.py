a286d15959408c2e4028b9b4613e292e3432e87f
Tulio Fernandes <tuliofalmeida@hotmail.com>
2021-05-04 10:50:04 -0300

    Docs

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
                       'matplotlib'],
     license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
